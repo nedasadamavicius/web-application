d7c3c7fb404eac7c08ed6681f5d60e8b051981b5
nedasadamavicius <117280183+nedasadamavicius@users.noreply.github.com>
2024-07-19 16:56:15 +0200

    feat: added models for artist

--- WebProject/WebApplication/views.py
+++ WebProject/WebApplication/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from .models import Artist
 
 # Create your views here.
+
+# Collection of artists in the database
 def home_view(request):
-    return render(request, 'WebApplication/home.html')
+    artists = Artist.objects.all()
+    return render(request, 'WebApplication/home.html', {'artists': artists})
 
-
+# User profile page
 def profile_view(request):
     return render(request, 'WebApplication/profile.html')
 
-
+# Individual artist page
 def artist_view(request):
     return render(request, 'WebApplication/artist.html')
