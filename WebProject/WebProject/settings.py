3af674fbbe36b4b6e72a674ea805028f9aeeed64
pythonanywhere nedasadamavicius <nedasadamavicius@pythonanywhere.com>
2024-07-22 16:14:00 +0000

    feat: updated settings

--- WebProject/WebProject/settings.py
+++ WebProject/WebProject/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
